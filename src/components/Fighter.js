e43db1d364352c70f1147223ff1b00e09aeb63ce
Jerry <Jvictor940@gmail.com>
2022-04-05 13:04:48 -0400

    added figher imgs

--- src/components/Fighter.js
+++ src/components/Fighter.js
@@ -1,6 +1,6 @@
-export default ({ name, color }) => {
+export default ({ name, color, img }) => {
     return (
-        <div className="fighter" style={{backgroundColor: color}}>
+        <div className="fighter" style={{backgroundColor: color, backgroundImage: `url(https://www.smashbros.com/assets_v2/img/fighter/${name.toLowerCase()}/main.png)`}}>
             <h4>{name}</h4>
         </div>
     )
