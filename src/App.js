cd55a1c7d7720df857a430439a86536de05f8677
Jerry <Jvictor940@gmail.com>
2022-04-05 13:15:23 -0400

    created fighter screen

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ function App() {
         <Fighter name ={'Roy'} color={'red'} />
         <Fighter name ={'Fox'} color={'green'} />
         <Fighter name ={'Ganondorf'} color={'purple'} />
+        <Fighter name ={'Sheik'} color={'lightskyblue'} />
+      </div>
+      <div>
+        <div className='fighter-screen'></div>
+        <h2>Roy</h2>
+        <img src="https://www.smashbros.com/assets_v2/img/fighter/roy/main.png" />
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse alias cumque labore minima in, sed nam, sequi ipsum aut libero perspiciatis, distinctio voluptatibus corrupti! Ab qui necessitatibus quia laborum iusto.
+        </p>
       </div>
     </div>
   );
